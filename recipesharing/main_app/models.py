c32f4b391ba79cabaf8e933008a60734749bcdbf
Hussain-Bader <7ussain.3ali@gmail.com>
2025-08-26 14:11:30 +0300

    main page that show list of recipes

--- recipesharing/main_app/models.py
+++ recipesharing/main_app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
-    ingredients = models.TextField()
+    description = models.TextField(max_length=200)
+    ingredients = models.TextField(max_length=2)
     image = models.ImageField(upload_to='main_app/static/uploads/', default="")
 
-    def __str__(self):
-        return self.title
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'recipe_id': self.id})
--- recipesharing/main_app/views.py
+++ recipesharing/main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Recipe
 # Create your views here.
 def home(request):
     # return HttpResponse('<h1>Hello Cat Collector</h1>')
     return render(request, 'home.html')
+
+def recipes_index(req):
+    recipes = Recipe.objects.all()
+    return render(req,'recipes/index.html' , {'recipes': recipes})
