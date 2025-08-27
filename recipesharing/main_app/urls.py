8eccbbccf4432a441cc1b7942ce8ca6bcac7249b
Hussain-Bader <7ussain.3ali@gmail.com>
2025-08-27 13:31:41 +0300

    now user can edit and delete recipe

--- recipesharing/main_app/urls.py
+++ recipesharing/main_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/', views.recipes_index , name='index'),
-    path('recipess/<int:recipe_id>/', views.recipes_detail, name='detail')
+    path('recipess/<int:recipe_id>/', views.recipes_detail, name='detail'),
+
+    path('recipes/create/', views.RecipeCreate.as_view(), name='recipes_create'),
+    path('recipes/<int:pk>/update/', views.RecipeUpdate.as_view(), name='recipes_update'),
+    path('recipes/<int:pk>/delete/', views.RecipeDelete.as_view(), name='recipes_delete'),
 ]
--- recipesharing/main_app/views.py
+++ recipesharing/main_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from .models import Recipe
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
+
+class RecipeCreate(CreateView):
+    model=Recipe
+    fields=['title','description','image','ingredients']
+
+class RecipeUpdate(UpdateView):
+    model=Recipe
+    fields=['title','description','ingredients']
+
+class RecipeDelete(DeleteView):
+    model = Recipe
+    success_url = '/recipes/'
+
 def home(request):
     # return HttpResponse('<h1>Hello Cat Collector</h1>')
     return render(request, 'home.html')
